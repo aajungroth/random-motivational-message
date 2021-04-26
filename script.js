7b98402a37e376849b0e8488832af319ce9c6c2d
aajungroth <aajungroth@ucdavis.edu>
2021-04-26 16:40:32 -0700

    Add message parts in arrays

--- script.js
+++ script.js
@@ -1,2 +1,40 @@
+// The message parts
+const messagePart1 = [
+  'Every new day',
+  'Always remember to',
+  'Thinking positively',
+  'When the going gets tough',
+  'Remember the good times,',
+  'Today is a new day to',
+  'Feel good,',
+  'Practice makes perfect,',
+  'Have a good attitude,',
+  'Cheer up,',
+];
+const messagePart2 = [
+    'be happy,',
+    'dream big,',
+    'keep believing,',
+    'put a smile on your face,',
+    'there is more than one way,',
+    'always be grateful,',
+    'believe in yourself,',
+    'touch the sky,',
+    'hustle,',
+    'work hard,',
+];
+const messagePart3 = [
+    'you can do it.',
+    'start today.',
+    'the best is ahead of us.',
+    'you only live once.',
+    'learn, live, love.',
+    'life is good.',
+    'just do it.',
+    'Booyah!',
+    'the early bird gets the worm.',
+    'you have limitless potential.',
+];
+
 // Introduction message
 console.log('Motivational Message Incoming!');
